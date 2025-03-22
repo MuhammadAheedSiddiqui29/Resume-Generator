929b0c84168df43ef6c3c4998be04a20a738aa43
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-03-22 07:46:30 +0500

    js functionality

--- script.js
+++ script.js
@@ -30,4 +30,80 @@ function addNewAQField() {
     aqOb.insertBefore(newNode, aqAddButtonOb);
 }
 
+                                        // GENERATING CV
 
+function generateCV() {
+
+                                        // NAME FUNCTIONALITY
+    let nameField = document.getElementById("nameField").value;
+    let nameT1 = document.getElementById("nameT1");
+    let nameT2 = document.getElementById("nameT2");
+
+    nameT1.innerHTML = nameField;
+    nameT2.innerHTML = nameField;
+
+                                        // CONTACT FUNCTIONALITY
+
+    let contactField = document.getElementById("contactField").value;
+    let contactT = document.getElementById("contactT");
+
+    contactT.innerHTML = contactField;
+
+                                        // ADDRESS FUNCTIONALITY
+
+    let addressField = document.getElementById("addressField").value;
+    let addressT = document.getElementById("contactT");
+
+    addressT.innerHTML = addressField;
+
+                                        // FACEBOOK FUNCTIONALITY
+
+    let fbField = document.getElementById("fbField").value;
+    let fbT = document.getElementById("fbT");
+
+    fbT.innerHTML = fbField;
+
+                                        // INSTA FUNCTIONALITY
+
+    let instaField = document.getElementById("instaField").value;
+    let instaT = document.getElementById("instaT");
+
+    instaT.innerHTML = instaField;
+
+                                        // LINKEDIN FUNCTIONALITY
+
+    let linkedField = document.getElementById("linkedField").value;
+    let linkedT = document.getElementById("linkedT");
+
+    linkedT.innerHTML = linkedField;
+
+                                        // OBJECTIVE FUNCTIONALITY
+
+    let objectiveField = document.getElementById("objectiveField").value;
+    let objectiveT = document.getElementById("objectiveT");
+
+    objectiveT.innerHTML = objectiveField;
+
+                                        // WORK EXPERIENCE FUNCTIONALITY
+
+    let wes = document.getElementsByClassName("weField");
+    let str1 = '';
+
+    for (let e of wes) {
+        str1 = str1 + `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("weT").innerHTML = str1;
+
+                                       // ACADEMIC QUALIFICATION FUNCTIONALITY
+
+
+    let aqs = document.getElementsByClassName("eqField");
+    let str2 = '';
+
+    for (let e of aqs) {
+        str2 = str2 + `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("aqT").innerHTML = str2;
+}
